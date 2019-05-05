b0036053dcd2cf53ca31a3b9ff1ed143135721f2
xuxm <xuxm@primeton.com>
2019-05-05 20:21:09 +0800

    add test

--- src/com/xxm/Test.java
+++ src/com/xxm/Test.java
@@ -34,6 +34,35 @@ public class Test {
                 }
             },"producer:" + i).start();
         }
+
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        LockContainer<String> container1 = new LockContainer<>();
+        for (int i=0;i<10;i++){
+            new Thread(()->{
+                for(int j=0;j<5;j++){
+                    System.out.println(container1.get());
+                }
+            },"consumer1" + i).start();
+        }
+
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        for (int i=0;i<2;i++){
+            new Thread(()->{
+                for (int j=0;j<25;j++){
+                    container1.put(Thread.currentThread().getName()+" "+j);
+                }
+            },"producer1:" + i).start();
+        }
     }
 
 }
